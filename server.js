0227e6e767cb424d4db2fa131bd03f20ccb2272d
David_Arruda <david.viniciusarruda@gmail.com>
2025-08-05 21:37:27 -0300

    Avançando com node.js

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-import express from 'express'
+import express, { json } from 'express'
 
 const app  = express()
+app.use(express.json())
+const users = []
 
+app.get('/users/', (req, res) => {
+    res.status(200).json(users)
+})
 
-
-
-
-
+app.post('/users', (req, res) => {
+    users.push(req.body)
+    res.status(201).json({message : "Usuario criado com sucesso"})
+})
 
 app.listen(3000)
+//req -> requisição
+//res -> resposta
+// http://localhost:3000
