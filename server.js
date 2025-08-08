7b055ed0ef8580b2854b598bd4d2c09f8fc3a197
David_Arruda <david.viniciusarruda@gmail.com>
2025-08-07 22:12:41 -0300

    Criando CRUD em Js

--- server.js
+++ server.js
@@ -3,23 +3,59 @@ import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
-const app  = express()
+const app = express()
 app.use(express.json())
-const users = []
 
-app.get('/users/', (req, res) => {
+
+app.post('/users', async (req, res) => {
+    const user = await prisma.user.create({
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+    })
+    console.log(user);
+    res.status(201).json({ message: "Usuario criado com sucesso" })
+})
+
+
+app.get('/users/', async (req, res) => {
+    const users = await prisma.user.findMany()
     res.status(200).json(users)
 })
 
-app.post('/users', (req, res) => {
-    users.push(req.body)
-    res.status(201).json({message : "Usuario criado com sucesso"})
+
+
+app.put('/users/:id', async (req, res) => {
+    req.params.id
+    const user = await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            age: req.body.age,
+            name: req.body.name
+        }
+    })
+    console.log(user);
+    res.status(200).json({ message: "Usuario atualizado com sucesso" })
 })
 
+
+app.delete('/users/:id', async (req, res) => {
+    await prisma.user.delete({
+        where: {
+            id: req.params.id,
+        },
+    })
+    res.status(200).json({ message: "Usuário deletado com sucesso!" })
+})
+
+
 app.listen(3000)
-//req -> requisição
-//res -> resposta
-// http://localhost:3000
+
 
 //davidviniciusarruda
 //bjE48z4TUACZzOuj
