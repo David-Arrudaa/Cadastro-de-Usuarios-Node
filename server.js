b24a54cedba7f918c4a7cebd3605cc1a6a28882d
David_Arruda <david.viniciusarruda@gmail.com>
2025-08-10 19:51:46 -0300

    Adiciona aula de try/catch no modulo avançado de Js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 import express from 'express'
+
+//URM
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
@@ -6,8 +8,9 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-
+//CREAT
 app.post('/users', async (req, res) => {
+    try{
     const user = await prisma.user.create({
         data: {
             email: req.body.email,
@@ -17,16 +20,18 @@ app.post('/users', async (req, res) => {
     })
     console.log(user);
     res.status(201).json({ message: "Usuario criado com sucesso" })
+} catch (err) {
+    return res.status(500).json({error : "Internal server error" });
+}
 })
 
-
+// READ
 app.get('/users/', async (req, res) => {
     const users = await prisma.user.findMany()
     res.status(200).json(users)
 })
 
-
-
+//UPDATE
 app.put('/users/:id', async (req, res) => {
     req.params.id
     const user = await prisma.user.update({
@@ -43,7 +48,7 @@ app.put('/users/:id', async (req, res) => {
     res.status(200).json({ message: "Usuario atualizado com sucesso" })
 })
 
-
+//DELETE
 app.delete('/users/:id', async (req, res) => {
     await prisma.user.delete({
         where: {
